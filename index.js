43997f9eccc153feb48874cceed50e0f26b2e0df
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-04-19 07:36:34 +0100

    controllers updated

--- index.js
+++ index.js
@@ -29,7 +29,12 @@ app.use('/api', movie)
  */
 const port = process.env.PORT || '3000'
 app.set('port', port)
-
+// Cross Origin middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 /**
  * Create HTTP server.
  */
